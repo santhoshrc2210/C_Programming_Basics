03b81b08be91628e4356340e5ec113aa95001acb
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-06-28 17:21:45 +0530

    Create factorial_num.c

---
+++ Loops/factorial_num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    int a,num_fact,num;
+    printf("Enter the number:");
+    scanf("%d",&num);
+    num_fact=1;
+    for (a=1;a<=num;++a)
+        num_fact=num_fact*a;
+    printf("The factorial of the number is:%d\n",num_fact);
+}

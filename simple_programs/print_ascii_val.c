2dc8673f80eacd8b69ff26ea0abcc2b899f73f3d
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-07-20 17:14:40 +0530

    Create print_ascii_val.c

---
+++ simple_programs/print_ascii_val.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    char crtr;
+    int i_crtr;
+    printf("Enter character:");
+    scanf("%c",&crtr);
+    i_crtr=crtr;
+    printf("ASCII value of character is:%d\n",i_crtr);
+    return 0;
+}

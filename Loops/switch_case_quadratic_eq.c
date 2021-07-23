56b592347a49cd3977e7c83d91bf31ffd6dc7700
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-07-23 13:34:54 +0530

    Create switch_case_quadratic_eq.c

---
+++ Loops/switch_case_quadratic_eq.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+    //equation in the form ax2+bx+c=0
+    //lf is for double that is more precise
+    int n;
+    double a,b,c;
+    double det,root1,root2,realPart,imagPart;
+    printf("Enter the coeffecients:\n");
+    scanf("%lf%lf%lf",&a,&b,&c);
+    det=((b*b)-4*a*c);
+    n=det>0;
+    switch (n){
+        case 1:
+            root1= (-b+sqrt(det))/(2*a);
+            root2= (-b-sqrt(det))/(2*a);
+            printf("root1:%lf,root2:%lf\n",root1,root2);
+            break;
+        
+        case 0:
+            if (det==0){
+                root1=-b;
+                root2=-b;
+                printf("root1:%lf,root2:%lf\n",root1,root2);
+
+            }
+            else if (det<0){
+                realPart = -b / (2 * a);
+                imagPart = sqrt(-det) / (2 * a);
+                printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi\n", realPart, imagPart, realPart, imagPart);
+                    }
+            
+    }
+}

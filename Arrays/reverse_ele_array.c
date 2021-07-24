b2597b75f9ef2bd84315257713bed6f0acb82099
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-07-24 12:46:18 +0530

    Create reverse_ele_array.c

---
+++ Arrays/reverse_ele_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // without using a new array
+    int arr[10],temp,i,len;
+    
+    //for array index starts from zero to len-1
+    printf("Enter the length of array:");
+    scanf("%d",&len);
+    //initialize array with inputs from user
+    printf("Enter elements of array:");
+    for (i=0;i<len;i++){
+        scanf("%d",&arr[i]);
+    }
+    //reverse array by swapping the elements only for one half of the array
+    for (i=0;i<len/2;i++){
+        temp=arr[i];
+        arr[i]=arr[(len-1)-i];
+        arr[(len-1)-i]=temp;
+        //printf("%d%d",arr[i],arr[len-1-i]);
+    }
+    //print the resulting array
+    for (i=0;i<len;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}

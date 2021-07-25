9d84008197b9692f784bcffc0723b67b72b34698
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-07-25 13:42:45 +0530

    Create print_1ton_no_semicolon.c

---
+++ Puzzles/print_1ton_no_semicolon.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+#define N 20
+
+int main(int num, char *argv[]) {
+   while (num <=N && printf("%d ", num) && num++) {
+      //The while body is empty.
+   }
+}
+
+//https://www.tutorialspoint.com/c-program-to-print-numbers-from-1-to-n-without-using-semicolon

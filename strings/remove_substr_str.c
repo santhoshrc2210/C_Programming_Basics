4a2ce7114f0d0cfe9cb6acfb895b5639b4abe5c8
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-07-27 15:28:06 +0530

    Update remove_substr_str.c

--- strings/remove_substr_str.c
+++ strings/remove_substr_str.c
@@ -1,32 +1,34 @@
-#include <stdio.h>
 
+#include <stdio.h>
+//try without using new str
 int main() {
-    // insert code here...
-    char str[20],sub_str[20],new_str[20];
-    int i=0,j=0,idx=0;
-    printf("Enter string and substring:\n");
+    char str[100],sub_str[100],new_str[100];
+    int i=0,j=0,idx=0,k=0,sub_str_present=0,i_pos=0,j_pos=0;
+    printf("Enter string and sub-string:");
     scanf("%s %s",str,sub_str);
-    
-    //first check if string is present and the index in the str
+    //check substring is present
     for (i=0;str[i]!='\0';i++){
+        j=0;//needs to reset, start from the first pos of substring
         while (sub_str[j]==str[i]){
-            i++;
             j++;
+            i++;
             if (sub_str[j]=='\0'){
-                idx=i-j;
-                //printf("The sub string is present \n");
+                sub_str_present=1;
+                i_pos=i;
+                j_pos=j;
             }
+    }
+    }
+    //printf("%d %d\n",i_pos,j_pos);
+    if (sub_str_present==1){
+        for (idx=0;str[idx]!='\0';idx++){
+            if ((idx<(i_pos-j_pos)) || (idx>=i_pos)){
+                new_str[k]=str[idx];
+                //printf("%d",k);
+                k++;
         }
     }
-    //remove by skipping past the substring and copy to new string
-    i=0;
-    int k=0;
-    for (i=0;str[i]!='\0';i++){
-        if (i<=idx-1 || i>=idx+j){
-            new_str[k]=str[i];
-            k++;
-        }
     }
-    printf("%s\n",new_str);
-            
+    printf("\n%s\n",new_str);
 }
+

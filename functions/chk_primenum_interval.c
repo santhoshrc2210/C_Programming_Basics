32caa7f94ce9a4e055ec644bf8cc6ad0246ae1f9
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-07-28 13:11:03 +0530

    Update chk_primenum_interval.c

--- functions/chk_primenum_interval.c
+++ functions/chk_primenum_interval.c
@@ -1,35 +1,30 @@
-//  check_prime_intervals for any number greater than 2
-
 #include <stdio.h>
-
-int check_prime(int num);
-
-int main() {
-    int min_num=0,max_num=0,num=0,num_prime;
-    printf("Enter the two numbers:\n");
-    scanf("%d%d",&min_num,&max_num);
-    
-    for (num=min_num;num<max_num;num++){
-        num_prime=check_prime(num);
-        if (num_prime==1){
-            printf("%d ",num);
-        }
-        
-    }
-}
-int check_prime(int num){
-    int i_num=2,num_factors=0;
-    //  Prime number has factors only 1 and itself
-    for (i_num=2;i_num<num;i_num++){
-        if (num%i_num==0){
-            num_factors++;
-        }
-    }
-    if (num_factors==0){
+//function to check if a number is prime returns 0 if number is prime otherwise returns 1
+//prime number has a factor of 1 and itself (2,3,5,7,11....
+int chk_prime(int num){
+    int i=1;
+    if (num==1){
         return 1;
     }
-    else{
-        return 0;
+    else
+        for (i=2;i<num;i++){
+            if (num%i==0){
+                return 1;
+        }
     }
-    
+    return 0;
 }
+
+int main() {
+    int num_first,num_last,num,is_prime;
+    printf("Enter the first and last number of the interval:");
+    scanf("%d %d",&num_first,&num_last);
+    for (num=num_first;num<=num_last;num++){
+        is_prime=chk_prime(num);
+        if (is_prime==0){
+            printf("%d ",num);
+        }
+    }
+    printf("\n");
+}
+

59111de1206a91642f049cad0bac836fdc2ecbd0
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-07-30 15:03:33 +0530

    Create gcd_num_recursion.c

---
+++ functions/gcd_num_recursion.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+//
+//using euclids recursive algorithm
+int hcf(int num1, int num2){
+    int smallest;
+    //base case
+    if (num2==0){
+        return num1;
+    }
+    else if (num1==0){
+        return num2;
+    }
+    else if (num1==num2){
+        return num1;
+    }
+    //recursive case
+    else
+        //the difference of the two nums and the smallest number
+        if (num1>num2){
+            smallest=num2;
+            num1=num1-num2;
+            return hcf(num1,num2);
+        }
+        else
+            smallest=num1;
+            num2=num2-num1;
+            return hcf(num2,num1);
+    
+}
+
+int main() {
+    int num1,num2;
+    printf("Enter the two numbers:");
+    scanf("%d%d",&num1,&num2);
+    printf("%d\n",hcf(num1,num2));
+    return 0;
+}

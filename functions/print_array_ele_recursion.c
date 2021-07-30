b127268f33beb3cefc4bf6f7109c03d7b72b8efa
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-07-30 10:58:05 +0530

    Create print_array_ele_recursion.c

---
+++ functions/print_array_ele_recursion.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#define MAX_SIZE 100
+
+/* Function declaration */
+void printArray(int arr[], int start, int len);
+
+
+int main()
+{
+    int arr[MAX_SIZE];
+    int N, i;
+    
+    /* Input size and elements in array */
+    printf("Enter size of the array: ");
+    scanf("%d", &N);
+    printf("Enter elements in the array: ");
+    for(i=0; i<N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+        
+    /* Prints array recursively */
+    printf("Elements in the array: ");
+    printArray(arr, 0, N);
+    
+    return 0;
+}
+
+
+/**
+ * Prints an array recursively within a given range.
+ */
+void printArray(int arr[], int start, int len)
+{
+    /* Recursion base condition */
+    if(start >= len)
+        return;
+        
+    /* Prints the current array element */
+    printf("%d, ", arr[start]);
+    
+    /* Recursively call printArray to print next element in the array */
+    printArray(arr, start + 1, len);
+}
+
+//https://codeforwin.org/2016/03/c-program-to-print-elements-of-array-using-recursion.html
